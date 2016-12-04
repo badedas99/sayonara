6639b04f264169d32f0eb7b6542c2da6b564083a
badedas99 <lukas.hoelscher@t-online.de>
2016-12-04 18:09:01 +0100

    removed erstaunlicherweise, add nach Adam Riese

--- index.js
+++ index.js
@@ -5,4 +5,4 @@ const number1 = process.argv[2];
 const number2 = process.argv[3];
 const result = Number(number1) + Number(number2);
 
-execa.sync('say', [`${number1} plus ${number2} ist erstaunlicherweise ${result}`]);
+execa.sync('say', [`${number1} plus ${number2} ist nach Adam Riese ${result}`]);
