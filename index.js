b8a71316537eeeb6eb870da699f61b8f1c322de1
badedas99 <lukas.hoelscher@t-online.de>
2016-12-04 18:21:25 +0100

    changed plus into multiply

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const execa = require('execa');
 // Rechnen
 const number1 = process.argv[2];
 const number2 = process.argv[3];
-const result = Number(number1) + Number(number2);
+const result = Number(number1) * Number(number2);
 
-execa.sync('say', [`${number1} plus ${number2} ist nach Adam Riese ${result}`]);
+execa.sync('say', [`${number1} mal ${number2} ist nach Adam Riese ${result}`]);
