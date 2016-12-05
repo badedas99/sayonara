ba38cef167d929d6aa74002c51a348db11adde7e
badedas99 <lukas.hoelscher@t-online.de>
2016-12-05 20:51:04 +0100

    Nichts funktioniert so wie es soll, lol

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const execa = require('execa');
-
-// Rechnen
+// input shit
+const operationType = process.argv[3]
 const number1 = process.argv[2];
-const number2 = process.argv[3];
+const number2 = process.argv[4];
+
+// calculation shit
+if (operationType === "plus") {
+  const result = Number(number1) + Number(number2);
+} else if (operationType === "minus") {
+  const result = Number(number1) - Number(number2);
+} else if (operationType === "mal") {
+  const result = Number(number1) * Number(number2);
+} else if (operationType === "geteilt") {
+  const result = Number(number1) * Number(number2);
+};
+// result shit
 const result = Number(number1) * Number(number2);
 
 execa.sync('say', [`${number1} mal ${number2} ist nach Adam Riese ${result} ... krank`]);
