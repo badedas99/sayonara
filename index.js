c0c4b57d8d32ba6513ee0281c6337b4a74315f20
badedas99 <lukas.hoelscher@t-online.de>
2016-12-05 01:02:24 +0100

    add krank

--- index.js
+++ index.js
@@ -5,4 +5,4 @@ const number1 = process.argv[2];
 const number2 = process.argv[3];
 const result = Number(number1) * Number(number2);
 
-execa.sync('say', [`${number1} mal ${number2} ist nach Adam Riese ${result}`]);
+execa.sync('say', [`${number1} mal ${number2} ist nach Adam Riese ${result} ... krank`]);
